acbecf547c02f4a9b913e56e48a0b75aa933ba7b
michalkaminski <m.kaminski@clawrock.com>
2024-11-05 18:42:50 +0100

    modify request rules to contain digits only

--- app/Http/Requests/StoreCompanyRequest.php
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -9,10 +9,9 @@ class StoreCompanyRequest extends FormRequest
 {
     public function rules(): array
     {
-        // TODO: Add validation for nip formating
         return [
             'name' => 'required|string',
-            'nip' => 'required|string|size:10|unique:companies',
+            'nip' => 'required|string|digits:10|unique:companies',
             'address' => 'required|string',
             'city' => 'required|string',
             'postal_code' => 'required|string|regex:/^\d{2}-\d{3}$/',
--- app/Http/Requests/UpdateCompanyRequest.php
+++ app/Http/Requests/UpdateCompanyRequest.php
@@ -9,10 +9,9 @@ class UpdateCompanyRequest extends FormRequest
 {
     public function rules(): array
     {
-        // TODO: Add validation for nip formating
         return [
             'name' => 'sometimes|required|string',
-            'nip' => 'sometimes|required|string|size:10|unique:companies,nip,' . $this->company,
+            'nip' => 'sometimes|required|string|digits:10|unique:companies,nip,' . $this->company,
             'address' => 'sometimes|required|string',
             'city' => 'sometimes|required|string',
             'postal_code' => 'sometimes|required|string|regex:/^\d{2}-\d{3}$/',
