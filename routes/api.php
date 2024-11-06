2b5ee7d51c05edd320aa6a5c887fb7216b54dad2
michalkaminski <m.kaminski@clawrock.com>
2024-11-06 19:20:20 +0100

    add employees route

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('companies', CompanyController::class);
+Route::apiResource('employees', EmployeeController::class);
 
 Route::fallback(function () {
     abort(404, 'API resource not found');
