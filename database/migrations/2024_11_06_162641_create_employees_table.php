cd5c16cbd6bf07013e787f9b9d4a5f93b50877b7
michalkaminski <m.kaminski@clawrock.com>
2024-11-06 17:33:21 +0100

    create migration for employee model

---
+++ database/migrations/2024_11_06_162641_create_employees_table.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('first_name');
+            $table->string('second_name');
+            $table->string('email')->unique();
+            $table->string('phone')->nullable();
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
