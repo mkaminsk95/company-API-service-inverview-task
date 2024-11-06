087916f589216d5316497a7968ecbf8cca07284f
michalkaminski <m.kaminski@clawrock.com>
2024-11-06 11:59:19 +0100

    adjust code to PhpStan standards

--- app/Http/Requests/StoreCompanyRequest.php
+++ app/Http/Requests/StoreCompanyRequest.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreCompanyRequest extends FormRequest
 {
+    /**
+     * @return string[]
+     */
     public function rules(): array
     {
         return [
