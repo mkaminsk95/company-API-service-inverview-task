1bd35ba1d8f8436852066b4ed370655f2bb7b85e
michalkaminski <m.kaminski@clawrock.com>
2024-11-06 20:31:19 +0100

    modify validation rules

--- app/Http/Requests/StoreEmployeeRequest.php
+++ app/Http/Requests/StoreEmployeeRequest.php
@@ -8,16 +8,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreEmployeeRequest extends FormRequest
 {
+    const PHONE_REGEX = "/^([0|\+[0-9]{1,5})?([0-9]{9})$/";
+
     /**
      * @return string[]
      */
     public function rules(): array
     {
         return [
-            'first_name' => 'required|string:255',
-            'second_name' => 'required|string:255',
-            'email' => 'required|string:255|email|unique:employees',
-            'phone' => 'sometimes|required|string|digits:15|unique:employees',
+            'first_name' => 'required|string|max:255',
+            'second_name' => 'required|string|max:255',
+            'email' => 'required|string|max:255|email|unique:employees',
+            'phone' => ['sometimes', 'required', 'string', 'regex:' . self::PHONE_REGEX, 'unique:employees'],
             'company_id' => 'required|exists:companies,id',
         ];
     }
--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -8,16 +8,18 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateEmployeeRequest extends FormRequest
 {
+    const PHONE_REGEX = "/^([0|\+[0-9]{1,5})?([0-9]{3,9})$/";
+
     /**
      * @return string[]
      */
     public function rules(): array
     {
         return [
-            'first_name' => 'sometimes|required|string:255',
-            'second_name' => 'sometimes|required|string:255',
-            'email' => 'sometimes|required|string:255|email|unique:employees,email,'.$this->employee->email,
-            'phone' => 'sometimes|required|string|digits:15|unique:employees'.$this->employee->phone,
+            'first_name' => 'sometimes|required|string|max:255',
+            'second_name' => 'sometimes|required|string|max:255',
+            'email' => 'sometimes|required|string|max:255|email|unique:employees,email,'.$this->employee,
+            'phone' => ['sometimes', 'required', 'string', 'regex:' . self::PHONE_REGEX, 'unique:employees,phone,'.$this->employee],
             'company_id' => 'sometimes|required|exists:companies,id',
         ];
     }
