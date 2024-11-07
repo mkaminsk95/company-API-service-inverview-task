7a1b6db4d59b01ed2ba71068ff126ce733c36e8f
michalkaminski <m.kaminski@clawrock.com>
2024-11-07 13:49:28 +0100

    add phone number mutator

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Http\Helpers\PhoneHelper;
 use Database\Factories\EmployeeFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Employee extends Model
 {
@@ -28,5 +30,15 @@ class Employee extends Model
     public function company(): BelongsTo
     {
         return $this->belongsTo(Company::class);
+    }
+
+    /**
+     * @return Attribute<string, string>
+     */
+    protected function phone(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => PhoneHelper::cleanPhone($value),
+        );
     }
 }
