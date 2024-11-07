8319dcc2bbe1727749397b2dec7931f8b8966abb
michalkaminski <m.kaminski@clawrock.com>
2024-11-07 13:49:18 +0100

    improve phone number validation

--- app/Http/Requests/StoreEmployeeRequest.php
+++ app/Http/Requests/StoreEmployeeRequest.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Http\Helpers\PhoneHelper;
 use Illuminate\Foundation\Http\FormRequest;
 
 class StoreEmployeeRequest extends FormRequest
 {
-    const PHONE_REGEX = "/^([0|\+[0-9]{1,5})?([0-9]{9})$/";
+    const PHONE_REGEX = "/^[\+]?[(]?[0-9]{0,3}[)]?[-\s]?[0-9]{0,4}[-\s]?[0-9]{0,4}[-\s]?[0-9]{0,4}$/";
 
     /**
      * @return array<string, array<int, string>|string>
@@ -19,8 +20,15 @@ class StoreEmployeeRequest extends FormRequest
             'first_name' => 'required|string|max:255',
             'second_name' => 'required|string|max:255',
             'email' => 'required|string|max:255|email|unique:employees',
-            'phone' => ['sometimes', 'required', 'string', 'regex:'.self::PHONE_REGEX, 'unique:employees'],
+            'phone' => ['sometimes', 'required', 'string', 'regex:'.phoneHelper::PHONE_VALIDATION_REGEX, 'unique:employees'],
             'company_id' => 'required|exists:companies,id',
         ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'phone' => phoneHelper::cleanPhone(strval($this->phone)),
+        ]);
     }
 }
--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Requests;
 
+use App\Http\Helpers\PhoneHelper;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateEmployeeRequest extends FormRequest
 {
-    const PHONE_REGEX = "/^([0|\+[0-9]{1,5})?([0-9]{3,9})$/";
+    const PHONE_REGEX = "/^[\+]?[(]?[0-9]{0,3}[)]?[-\s]?[0-9]{0,4}[-\s]?[0-9]{0,4}[-\s]?[0-9]{0,4}$/";
 
     /**
      * @return array<string, array<int, string>|string>
@@ -19,8 +20,15 @@ class UpdateEmployeeRequest extends FormRequest
             'first_name' => 'sometimes|required|string|max:255',
             'second_name' => 'sometimes|required|string|max:255',
             'email' => 'sometimes|required|string|max:255|email|unique:employees,email,'.$this->employee,
-            'phone' => ['sometimes', 'required', 'string', 'regex:'.self::PHONE_REGEX, 'unique:employees,phone,'.$this->employee],
+            'phone' => ['sometimes', 'required', 'string', 'regex:'.phoneHelper::PHONE_VALIDATION_REGEX, 'unique:employees,phone,'.$this->employee],
             'company_id' => 'sometimes|required|exists:companies,id',
         ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $this->merge([
+            'phone' => phoneHelper::cleanPhone(strval($this->phone)),
+        ]);
     }
 }
