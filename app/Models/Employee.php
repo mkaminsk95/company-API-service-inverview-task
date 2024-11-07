8eebaca690d31044085e57b4a8299ee34f79672e
michalkaminski <m.kaminski@clawrock.com>
2024-11-07 09:39:15 +0100

    add HasFactory trait to model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,11 +3,14 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employee extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'first_name',
         'second_name',
